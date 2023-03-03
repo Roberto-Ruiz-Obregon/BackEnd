e7fe13d1b6937f95aec586583d5e922de900f13e
CesarJimenezVilleda02 <A01703517@tec.mx>
2023-03-03 02:45:52 -0600

    Implemented the basic functionality of the user controller and an example of how to use the handlerfactory

---
+++ controllers/user.controller.js
@@ -0,0 +1,10 @@
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+const APIFeatures = require(`../utils/apiFeatures`);
+const User = require('../models/users.model');
+
+exports.getAllUsers = factory.getAll(User);
+exports.getUser = factory.getOne(User);
+exports.createUser = factory.createOne(User);
+exports.updateUser = factory.updateOne(User);
+exports.deleteUser = factory.deleteOne(User);
