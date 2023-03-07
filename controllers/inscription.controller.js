017dabf05962d83d41ab1e3296d5fc76e276fbe3
Edgar Ramirez <leramirezp@hotmail.com>
2023-03-07 00:19:27 -0600

    populated inscription model for getOne controller

--- controllers/inscription.controller.js
+++ controllers/inscription.controller.js
@@ -2,6 +2,6 @@ const factory = require('./handlerFactory.controller');
 const Inscription = require('../models/Inscriptions.model');
 
 exports.getAllInscriptions = factory.getAll(Inscription);
-exports.getInscription = factory.getOne(Inscription);
+exports.getInscription = factory.getOne(Inscription, ['user', 'course']);
 exports.createInscription = factory.createOne(Inscription);
 exports.deleteInscription = factory.deleteOne(Inscription);
--- models/inscriptions.model.js
+++ models/inscriptions.model.js
@@ -2,14 +2,16 @@ const mongoose = require('mongoose');
 
 const inscriptionSchema = new mongoose.Schema({
     user: {
-        type: ObjectID,
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
         required: [true, 'Campo de usuario necesario'],
     },
     course: {
-        type: ObjectID,
+        type: mongoose.Schema.ObjectId,
+        ref: 'Course',
         required: [true, 'Campo de curso necesario'],
     },
-});
+}, { timestamps: true });
 
 const Inscription = mongoose.model('Inscription', inscriptionSchema);
 
