7116986e725c6a19ca9150994ce7256d7bbae151
CesarJimenezVilleda02 <A01703517@tec.mx>
2023-03-09 19:52:06 -0600

    Completed setup for email transporter

--- utils/email.js
+++ utils/email.js
@@ -1,35 +1,38 @@
-/**
- * PENDING
- */
-
 const nodemailer = require('nodemailer');
 const pug = require('pug');
 const htmlToText = require('html-to-text');
 
-// la idea es que siemre quese quiera crear un email solo se importe esta clase y a traves de ella se
-// mande un correo, esta tendra un metodo quese llamara cada que un nuevo usuario se registre
+/* The above code is creating a class called Email. The constructor is taking in two parameters, user
+and url. The constructor is also setting the to, firstName, url, and from properties. The
+newTransport method is creating a new transport object based on the environment. The send method is
+rendering the html based on the template and subject. The send method is also defining the mail
+options and sending the email. The sendWelcome method is calling the send method and passing in the
+welcome template and subject. The sendPasswordReset method is calling the send method and passing in
+the passwordReset template */
 module.exports = class Email {
+    /**
+     * The constructor function is a special method for creating and initializing an object created
+     * within a class.
+     * @param user - The user object that contains the email and name of the user.
+     * @param url - The URL that the user will be sent to in order to reset their password.
+     */
     constructor(user, url) {
         this.to = user.email;
         this.firstName = user.name.split(' ')[0];
         this.url = url;
-        this.from = `Pablo César Jiménez Villeda <${process.env.EMAIL_FROM}>`;
+        this.from = `Asociacion Roberto Ruiz Obregon <${process.env.EMAIL_FROM}>`;
     }
 
-    // MIENTRAS NO ESTEMOS EN PRODUCCION QUEREMOS USAR MAILTRAP
+    /**
+     * It creates a new transport object using the nodemailer library.
+     *
+     * The transport object is used to send emails.
+     *
+     * The transport object is created using the host, port, username, and password that we set in our
+     * .env file.
+     * @returns A new instance of the nodemailer transport object.
+     */
     newTransport() {
-        if (process.env.NODE_ENV === 'production') {
-            // Sendgrid
-            return nodemailer.createTransport({
-                host: process.env.EMAIL_PRODUCTION_HOST,
-                port: process.env.EMAIL_PRODUCTION_PORT,
-                auth: {
-                    user: process.env.EMAIL_PRODUCTION_USER,
-                    pass: process.env.EMAIL_PRODUCTION_PASS,
-                },
-            });
-        }
-
         return nodemailer.createTransport({
             host: process.env.EMAIL_HOST,
             port: process.env.EMAIL_PORT,
@@ -40,13 +43,16 @@ module.exports = class Email {
         });
     }
 
-    // el metodo que va a hacer el sending, recibe el template y el sujeto
+    /**
+     * The function takes in a template and a subject, renders the template using the data from the
+     * object, defines the email options, creates a new transport and sends the email.
+     * @param template - The name of the template file that we want to use.
+     * @param subject - The subject of the email
+     */
     async send(template, subject) {
-        // render html based on pug template
-        // tenemos que usar el paquete de pug
         const html = pug.renderFile(
             `${__dirname}/../views/emails/${template}.pug`,
-            // como segunda opcion son los datos que populan el correo
+            // The second argument will be an object of data that will populate the template
             {
                 firstName: this.firstName,
                 url: this.url,
@@ -60,26 +66,32 @@ module.exports = class Email {
             to: this.to,
             subject,
             html,
-            // para mandar una opcion de texto necesitamos un paquete
             // npm i html-to-text
             text: htmlToText.fromString(html),
         };
 
         //  create transport and send email
-        //  await transporter.sendMail(mailOptions);
         await this.newTransport().sendMail(mailOptions);
-        console.log('Email sent');
     }
 
+    /**
+     * The function sendWelcome() is an asynchronous function that sends a welcome message to the user.
+     */
     async sendWelcome() {
         // esto va a ser una pug template
-        await this.send('welcome', 'Welcome to the natours family!');
+        await this.send(
+            'welcome',
+            'Bienvenido a la fmailia Roberto Ruiz obregon!'
+        );
     }
 
+    /**
+     * It sends a password reset email to the user.
+     */
     async sendPasswordReset() {
         await this.send(
             'passwordReset',
-            'Your password reset token (valid for only 10 minutes)'
+            'Recuperar contraseña (valido por solo 10 minutos)'
         );
     }
 };
