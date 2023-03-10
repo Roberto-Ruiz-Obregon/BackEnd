4834e59ea3009b18df00f4eaec5e64ebd451e2d7
CesarJimenezVilleda02 <A01703517@tec.mx>
2023-03-09 20:15:24 -0600

    email configuration in GCP set

--- utils/email.js
+++ utils/email.js
@@ -34,11 +34,14 @@ module.exports = class Email {
      */
     newTransport() {
         return nodemailer.createTransport({
-            host: process.env.EMAIL_HOST,
-            port: process.env.EMAIL_PORT,
+            service: 'gmail',
             auth: {
-                user: process.env.EMAIL_USERNAME,
-                pass: process.env.EMAIL_PASSWORD,
+                type: 'OAuth2',
+                user: process.env.MAIL_USERNAME,
+                pass: process.env.MAIL_PASSWORD,
+                clientId: process.env.OAUTH_CLIENTID,
+                clientSecret: process.env.OAUTH_CLIENT_SECRET,
+                refreshToken: process.env.OAUTH_REFRESH_TOKEN,
             },
         });
     }
