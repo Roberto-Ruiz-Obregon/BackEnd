9911e8bed73d0d93f7a092f63d542c6d972fbce9
CesarJimenezVilleda02 <A01703517@tec.mx>
2023-03-15 01:54:23 -0600

    Added course notification email template

--- models/topics.model.js
+++ models/topics.model.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
-const bcrypt = require('bcryptjs');
-const crypto = require('crypto');
 
 const topicsSchema = new mongoose.Schema({
     topic: {
         type: String,
         required: [true, 'Se necesita al menos un interés'],
-    }
+    },
 });
 
 const Topics = mongoose.model('Topics', topicsSchema);
