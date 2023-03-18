bad0f998ef3b3b0e754bb1bf1e02aa9fa9981de3
CesarJimenezVilleda02 <A01703517@tec.mx>
2023-03-18 02:36:42 -0600

    Created inscribeTo route on inscritions

--- controllers/inscription.controller.js
+++ controllers/inscription.controller.js
@@ -1,7 +1,12 @@
 const factory = require('./handlerFactory.controller');
 const Inscription = require('../models/inscriptions.model');
+const catchAsync = require('../utils/catchAsync');
 
 exports.getAllInscriptions = factory.getAll(Inscription);
 exports.getInscription = factory.getOne(Inscription, ['user', 'course']);
+
+// TODO: CREATE CUSTOM MIDDLEWARES TO TAKE INTO ACCOUNT BUSINESS RULES
 exports.createInscription = factory.createOne(Inscription);
 exports.deleteInscription = factory.deleteOne(Inscription);
+
+exports.inscribeTo = catchAsync(async (req, res, next) => {});
--- routes/inscription.route.js
+++ routes/inscription.route.js
@@ -7,8 +7,17 @@ const {
     getInscription,
     getAllInscriptions,
     deleteInscription,
+    inscribeTo,
 } = require(`${__dirname}/../controllers/inscription.controller.js`);
+const {
+    protect,
+    restrictTo,
+} = require(`${__dirname}/../controllers/authentication.controller.js`);
 
+router.use(protect);
+router.route('/incribeTo').post(restrictTo('User'), inscribeTo);
+
+router.use(restrictTo('Admin'));
 router.route('/').get(getAllInscriptions).post(createInscription);
 router.route('/:id').get(getInscription).delete(deleteInscription);
 
