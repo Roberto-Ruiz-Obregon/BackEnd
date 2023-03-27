2ef339c328718bcd9034bbd1711ff12ccc601387
CesarJimenezVilleda02 <A01703517@tec.mx>
2023-03-27 12:49:53 -0600

    Fixed programs model

--- models/programs.model.js
+++ models/programs.model.js
@@ -1,23 +1,25 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-const programSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Es necesario que el programa tenga nombre'],
-    },
-    descripcion : {
-        type: String,
-    },
-    imageUrl: { 
-        type: String,
-        validate: {
-            validator: value => (
-                /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i.test(v)
-            ),
+const programSchema = new mongoose.Schema(
+    {
+        programName: {
+            type: String,
+            required: [true, 'Es necesario que el programa tenga nombre'],
         },
-    }
-}, { timestamps: true });
+        descripcion: {
+            type: String,
+        },
+        imageUrl: {
+            type: String,
+            validate: {
+                validator: (value) =>
+                    /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i.test(value),
+            },
+        },
+    },
+    { timestamps: true }
+);
 
 const Program = mongoose.model('Program', programSchema);
 module.exports = Program;
