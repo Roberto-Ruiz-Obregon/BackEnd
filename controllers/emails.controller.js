3642a3d9281b15d9bb13a9476a96a199d146b105
CesarJimenezVilleda02 <A01703517@tec.mx>
2023-03-28 00:00:13 -0600

    Removed unused imports

--- controllers/emails.controller.js
+++ controllers/emails.controller.js
@@ -1,12 +1,9 @@
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
-const APIFeatures = require(`../utils/apiFeatures`);
-const factory = require('./handlerFactory.controller');
 const User = require('../models/users.model');
 const Email = require('../utils/email');
-const { request } = require('express');
-const { user } = require('firebase-functions/v1/auth');
 
+/* A function that is being exported. */
 exports.sendToEveryone = catchAsync(async (req, res, next) => {
     const usersToAlert = await User.find({ emailAgreement: true });
 
@@ -33,6 +30,7 @@ exports.sendToEveryone = catchAsync(async (req, res, next) => {
     });
 });
 
+/* A function that is being exported. */
 exports.sendByZone = catchAsync(async (req, res, next) => {
     const usersToAlert = await User.find({
         emailAgreement: true,
