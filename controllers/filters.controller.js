611e76cfbccefb08c174113de094ae83e76cc924
Fer Torres <fertorres638gmail.com>
2023-03-27 18:44:15 -0600

    aggregations controller added

---
+++ controllers/filters.controller.js
@@ -0,0 +1,39 @@
+const User = require('../models/users.model');
+const Topics = require('../models/topics.model');
+
+exports.filterTopics = async (req, res) => {
+    const { postalCode, topics } = req.body;
+  
+    try {
+      const topicIds = await Topics.find({ topic: { $in: topics } }).distinct('_id');
+ 
+      const filteredUsers = await User.aggregate([
+        {
+          $match: {
+            postalCode: postalCode,
+            topics: { $in: topicIds },
+          },
+        },
+        {
+          $lookup: {
+            from: 'topics',
+            localField: 'topics',
+            foreignField: '_id',
+            as: 'topics',
+          },
+        },
+      ]);
+  
+      res.status(200).json({
+        status: 'success',
+        data: filteredUsers,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        status: 'error',
+        message: 'Ocurrió un error al filtrar usuarios',
+      });
+    }
+  };
+  
