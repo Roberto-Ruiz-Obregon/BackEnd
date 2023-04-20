d497f1a6f8ac37ccbcfe24f155bb2eb3c0911225
amatt2b <a01703889@tec.mx>
2023-04-19 18:26:18 -0600

    MailCertificate&UserModelEducation

--- utils/email.js
+++ utils/email.js
@@ -18,18 +18,20 @@ options and sending the email. The sendWelcome method is calling the send method
 welcome template and subject. The sendPasswordReset method is calling the send method and passing in
 the passwordReset template */
 
+// Crea un nuevo objeto OAuth2
 const myOAuth2Client = new OAuth2(
-    process.env.OAUTH_CLIENTID,
-    process.env.OAUTH_CLIENT_SECRET,
-    "https://developers.google.com/oauthplayground"
-    )
+    process.env.OAUTH_CLIENTID,        // Identificador del cliente (Client ID)
+    process.env.OAUTH_CLIENT_SECRET,  // Secreto del cliente (Client Secret)
+    "https://developers.google.com/oauthplayground" // URI de redireccionamiento
+);
 
+// Configura las credenciales del cliente OAuth2, que incluyen un token de actualización (refresh token)
 myOAuth2Client.setCredentials({
-    refresh_token:process.env.OAUTH_REFRESH_TOKEN
-    });
+    refresh_token:process.env.OAUTH_REFRESH_TOKEN // Token de actualización (refresh token)
+});
 
-const myAccessToken = myOAuth2Client.getAccessToken()
-
+// Obtiene un nuevo token de acceso (access token) usando el objeto OAuth2 configurado anteriormente
+const myAccessToken = myOAuth2Client.getAccessToken();
 
 module.exports = class Email {
     /**
