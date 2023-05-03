b67aea266139181d4f8cc6b9a5c8694998f6ea53
CesarJimenezVilleda02 <A01703517@tec.mx>
2023-05-03 07:58:12 -0600

    Added email test

---
+++ tests/emailRoutes/email.post.test.js
@@ -0,0 +1,30 @@
+const request = require('supertest');
+const app = require('../../app');
+const { connectDB } = require('../config/databaseProd');
+const { createUser, createAdmin } = require('../config/dataBaseTestSetUp');
+
+const agent = request.agent(app);
+
+beforeAll(async () => {
+    await connectDB();
+    await createAdmin();
+    await createUser();
+    // Da acceso a las rutas protegidas y restringidas
+    await loginAdmin(agent, 'dummy_admin@gmail.com', 'contra123');
+});
+
+describe('Program gets APIFeatures', () => {
+    describe('Successful POST /v1/emails/emailToEveryone', () => {
+        test('successful', async () => {
+            const res = await agent.post('/v1/emails/emailToEveryone').send({
+                message: 'Este es un correo de prueba',
+                imageUrl:
+                    'https://www.bbva.com/wp-content/uploads/2017/08/bbva-balon-futbol-2017-08-11.jpg',
+                subject: 'Prueba de correo',
+            });
+
+            expect(res.statusCode).toEqual(201);
+            expect(res.body).toBeTruthy();
+        });
+    });
+});
